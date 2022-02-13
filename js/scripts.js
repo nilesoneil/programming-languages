4e87b6fe45d14a1aa4954feb95da4f7ae7a2e17c
Niles O'Neil <nilesoneil@gmail.com>
2022-02-13 07:15:39 -0800

    WIP, making sure logic is sound

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,26 @@
 $(document).ready(function(event) {
   $("form#survey").submit(function(event) {
+
     const dates = $("#dates").val();
     const cuisine = $("#cuisine").val();
     const pets = $("#pets").val();
-    
-    
-    
+    const gender = $("#gender").val();
+    const season =$("#season").val();
 
-
-    if (dates === "dinner" || cuisine === "thai") {
+    
+    if (dates === "dinner" || cuisine === "thai" && gender === "female") {
       $("#ruby").show();
       $("#javascript").hide();
       $("#rust").hide();
-    } else if (dates === "bowling" || cuisine === "italian") {
+    } else if (dates === "bowling" || cuisine === "italian" || gender === "male") {
       $("#ruby").hide();
       $("#javascript").show();
       $("#rust").hide();
-    } else if (dates === "movie" || cuisine === "mexican") {
+    } else if (dates === "movie" || cuisine === "mexican" && season === "winter") {
+      $("#ruby").hide();
+      $("#javascript").hide();
+      $("#rust").show();
+    } else if (gender === "female" && season === "winter") {
       $("#ruby").hide();
       $("#javascript").hide();
       $("#rust").show();
